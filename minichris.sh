8cb79e3afb05a3f68f817e9e34c7f58d10b48acb
Tosin Akinosho <takinosh@redhat.com>
2023-04-14 16:15:48 -0400

    updating code

--- minichris.sh
+++ minichris.sh
@@ -1,45 +1,166 @@
-#!/bin/bash -e
-# purpose: start up miniChRIS using podman-compose
+#!/bin/bash
 
+# -----------------------------------------------------------------------------
+#
+#                       TERMINAL OUTPUT COLOR HELPERS
+#
+# -----------------------------------------------------------------------------
 
-HERE="$(dirname "$(readlink -f "$0")")"
-VENV="$HERE/.venv"
-VERBOSE=true
-PLUGIN_INSTANCE_LABELS='org.chrisproject.miniChRIS=plugininstance'
+# override tput so that it doesn't run in environments which does not support color
 
+__REAL_TPUT=$(which tput)
 
-# print command to run before running in verbose mode
-function verb () {
-  if [ "$VERBOSE" = 'true' ]; then
-    set -x
+function tput () {
+  if ! [ -v CI ] && [[ "$TERM" = xterm* ]]; then
+    $__REAL_TPUT "$@"
   fi
-  $@ # run the given command
-  { set +x; } 2> /dev/null
 }
 
-# Wrapper for development version of podman-compose
-function podman-compose () {
-  { set +x; } 2> /dev/null
-  if ! [ -d "$VENV" ]; then
-    verb python3 -m venv "$VENV"
-  fi
-  source "$VENV/bin/activate"
-  if ! [ -f "$VENV/bin/podman-compose" ]; then
-    verb pip3 install https://github.com/containers/podman-compose/archive/devel.tar.gz
+# define output colors
+BOLD="$(tput bold)"
+DIM="$(tput dim)"
+UNDERLINE="$(tput smul)"
+RESET="$(tput sgr0)"
+RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
+
+# Apply styles to text
+function style_text () {
+  printf "$1%s$RESET" "$2"
+}
+
+
+# -----------------------------------------------------------------------------
+#
+#                               HELPER FUNCTIONS
+#
+# -----------------------------------------------------------------------------
+
+
+function minichris_chrisomatic () {
+  if ! [ -v CI ]; then
+    tty_flags=-it
   fi
 
-  export PODMAN_HOST="$(podman info --format '{{ .Host.RemoteSocket.Path }}')"
-  # export PODMAN_USERNS=keep-id
-  verb "$VENV/bin/podman-compose" "$@"
+  noisy_sh podman run --rm $tty_flags \
+    --pod $CUBE_POD \
+    --security-opt label=disable \
+    -v "\"$(realpath "$HERE")/chrisomatic.yml:/chrisomatic.yml:ro\"" \
+    -v "$SOCK:/var/run/docker.sock:rw" \
+    ghcr.io/fnndsc/chrisomatic:0.4.1 chrisomatic "$@"
+}
+
+function minichris_down () {
+  noisy_sh "cat $HERE/kube/*.yml $HERE/pfcon-podman.yml | exec podman kube down --force -"
+}
+
+function minichris_up () {
+  start_cube
+  start_pfcon
+  minichris_chrisomatic
+}
+
+function start_cube () {
+  watch_for_logs $INIT_CONTAINER &
+  noisy_sh "cat $HERE/kube/*.yml | podman kube play --replace -"
+  reap_background_wait
+}
+
+function start_pfcon () {
+  local hard_coded_sock=/run/user/1000/podman/podman.sock
+  noisy_sh "sed \"s#$hard_coded_sock#$SOCK#\" $HERE/pfcon-podman.yml | podman kube play --replace -"
+}
+
+# Follow the logs of a container which has not yet been created and/or started.
+# Returns the lock file for the waiting process, refer to run_in_background
+function watch_for_logs () {
+  wait_until_running $1
+  noisy_sh podman logs --follow $1
+}
+
+# Poll Podman until a container is running.
+function wait_until_running () {
+  while sleep 0.5; do
+    if [ -n "$(podman ps -f name=$1 -f status=running --quiet)" ]; then
+      break
+    fi
+  done
+}
+
+# Check whether there is a background job running.
+function is_bg_running () {
+  # we need to check twice:
+  # if bg finished, `jobs %%` will indicate "done" the first time, then error on the next.
+  # if bg running, `jobs %%` will indicate "running" twice.
+  # if no bg job, `jobs %%` will error on the first call.
+  jobs %% > /dev/null 2>&1 && jobs %% > /dev/null 2>&1
+}
+
+# If we're waiting in the background for the initContainer logs, stop and print a warning.
+function reap_background_wait () {
+  if is_bg_running; then
+    style_text "$YELLOW" "warning"
+    echo ": missed poll for initContainer '$INIT_CONTAINER'"
+    kill %%
+    return 1
+  elif [ "$rc" = 1 ]; then
+    die "poll for initContainer exited with code $rc"
+  fi
+
+}
+
+# Print the arguments and run them using sh.
+# Exits if the command fails.
+function noisy_sh () {
+  style_text "$DIM" "\$ $*"
+  echo
+  set -e
+  sh -ec "$*"
+  set +e
 }
 
 # Exit the program with an error message
 function die () {
-  printf "%s: " "$(tput setaf 1)error$(tput sgr0)"
+  style_text "$RED" "error: "
   echo "$@"
-  tput sgr0
   exit 1
 }
+
+# -----------------------------------------------------------------------------
+#
+#                                   CONSTANTS
+#
+# -----------------------------------------------------------------------------
+
+CUBE_POD=minichris-cube-pod
+INIT_CONTAINER=minichris-cube-pod-migratedb
+
+HERE="${0%/*}"
+SOCK="$(podman info --format '{{ .Host.RemoteSocket.Path }}')"
+
+USAGE="
+Run ChRIS on Podman.
+
+$(style_text "$BOLD$UNDERLINE" "Usage:") $0 [up|down|chrisomatic]
+
+$(style_text "$BOLD$UNDERLINE" "Commands:")
+
+  $(style_text "$BOLD" "up")             start or update ChRIS
+  $(style_text "$BOLD" "down")           stop ChRIS and delete data
+  $(style_text "$BOLD" "chrisomatic")    sync plugins from $HERE/chrisomatic.yml file to CUBE
+"
+
+
+# -----------------------------------------------------------------------------
+#
+#                               PRE-FLIGHT CHECKS
+#
+# -----------------------------------------------------------------------------
+
+# the helper function noisy_sh will misbehave if $HERE contains shell-sensitive syntax
+if [[ "$HERE" = *" "* ]]; then
+  die "Directory '$HERE' contains space characters, please rename it!"
+fi
 
 if [ "$(podman info --format '{{ .Host.RemoteSocket.Exists }}')" != 'true' ]; then
   echo "$(tput setaf 1)error$(tput sgr0): podman socket must be active for pman."
@@ -50,24 +171,33 @@ if [ "$(podman info --format '{{ .Host.RemoteSocket.Exists }}')" != 'true' ]; th
   exit 1
 fi
 
-subcommand="${1:-start}"
+# -----------------------------------------------------------------------------
+#
+#                                 RUN COMMAND
+#
+# -----------------------------------------------------------------------------
+
+# show help and exit
+for arg in "$@"; do
+  if [[ "$arg" =~ ^-*h|help$ ]]; then
+    echo "$USAGE"
+    exit 0
+  fi
+done
+
+subcommand="${1:-up}"
 
 case "$subcommand" in
-  start )
-    podman-compose up -d
-    "$HERE/chrisomatic.sh" ;;
-  up)
-    podman-compose up -d ;;
-  dow*)
-    podman-compose down ;;
-  des*)
-    pls=$(podman ps -q -f 'label=org.chrisproject.miniChRIS=plugininstance')
-    [ -z "$pls" ] || verb podman rm -fiv $pls
-    podman-compose down -v
-    rm -rf "$VENV" ;;
-  com* | podman-compose)
-    shift
-    podman-compose "$@" ;;
+  u|up )
+    minichris_up
+    ;;
+  d|down )
+    minichris_down
+    ;;
+  c|chrisomatic )
+    minichris_chrisomatic
+    ;;
   *)
-    die "unrecognized subcommand $(tput bold)$subcommand";;
+    die "unrecognized command $(style_text "$BOLD" "$subcommand")"
+    ;;
 esac
